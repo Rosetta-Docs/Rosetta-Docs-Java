c89c206e72a18d66a4eb1c6eb99087af66b1584e
JabDoesThings <JabDoesThings@protonmail.com>
2025-11-27 12:45:41 -0600

    Created `JavaSerializeSettings`.

---
+++ src/main/java/com/asledgehammer/rosetta/java/JavaSerializeSettings.java
@@ -0,0 +1,58 @@
+package com.asledgehammer.rosetta.java;
+
+import com.asledgehammer.rosetta.exception.ReadOnlyException;
+import org.jetbrains.annotations.NotNull;
+
+public class JavaSerializeSettings {
+
+  private boolean readOnly = false;
+
+  private FullTypeRenderSettings fullTypeRenderSettings =
+      FullTypeRenderSettings.ONLY_WITH_MODIFIERS;
+
+  private boolean renderTypesAsDictionary = false;
+
+  public FullTypeRenderSettings getFullTypeRenderSettings() {
+    return fullTypeRenderSettings;
+  }
+
+  @NotNull
+  public JavaSerializeSettings setFullTypeRenderSettings(
+      @NotNull FullTypeRenderSettings fullTypeRenderSettings) {
+    checkReadOnlyStatus();
+    this.fullTypeRenderSettings = fullTypeRenderSettings;
+    return this;
+  }
+
+  public boolean isRenderTypesAsDictionary() {
+    return renderTypesAsDictionary;
+  }
+
+  @NotNull
+  public JavaSerializeSettings setRenderTypesAsDictionary(boolean flag) {
+    checkReadOnlyStatus();
+    this.renderTypesAsDictionary = flag;
+    return this;
+  }
+
+  public JavaSerializeSettings build() {
+    this.readOnly = true;
+    return this;
+  }
+
+  private void checkReadOnlyStatus() {
+    if (this.isReadOnly()) {
+      throw new ReadOnlyException("The JavaSerializeSettings is read-only.");
+    }
+  }
+
+  public boolean isReadOnly() {
+    return readOnly;
+  }
+
+  public enum FullTypeRenderSettings {
+    ALWAYS,
+    NEVER,
+    ONLY_WITH_MODIFIERS;
+  }
+}
